bedd87e22d39b725b320a636ed27bb0cfac01c24
ADIPRADIPTA <adipradipta93@gmail.com>
2019-03-09 02:07:10 +0700

    part2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
+$('body').scrollspy({target: ".navbar", offset: 50});
+
 // event pada saat link di klik
-$('.page-scroll').on('click', function(e) {
+$('.page-scroll').on('click', function(event) {
 
     //ambil isi href
-    var tujuan = $(this).attr('href');
+//    var tujuan = $(this).attr('href');
 
     // tangkap elemen ybs
-    var elemenTujuan = $(tujuan);
+//    var elemenTujuan = $(tujuan);
     
+    if (this.elemenTujuan !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault(); 
+        // Store hash
+        var tujuan = $(this).attr('href');
+        var elemenTujuan = $(tujuan);
     // pindahkan scroll
-    $('body').animate({
-        scrollTop: elemenTujuan.offset().top - 50
-    }, 1000, 'swing');
-
-    e.preventDefault();
+        $('html,body').animate({
+            scrollTop: elemenTujuan.offset().top - 50
+        }, 1000, function(){
+            window.location.elemenTujuan = elemenTujuan;
+        });
+        }
 });
